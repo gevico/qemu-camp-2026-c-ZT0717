72a8072fe8e6db27c5eec3b5fbda1f12dee6fb28
pmpmzt <1636932473@qq.com>
2026-04-16 18:47:07 +0800

    exec02 local test ok!

--- exercises/02_merge_sort/02_merge_sort.c
+++ exercises/02_merge_sort/02_merge_sort.c
@@ -13,9 +13,49 @@ typedef struct {
 Student students[MAX_STUDENTS];
 Student temp[MAX_STUDENTS];
 
+void merge(Student stu[], int L, int mid, int R) {
+    int i = L, j = mid + 1;
+    printf("i = %d, j = %d \n", i, j);
+    int index_t = 0;
+    // 降序，哪个大先放入temp
+    while (i <= mid && j <= R) {
+        if (stu[i].score >= stu[j].score) {
+            temp[index_t] = stu[i];
+            index_t++;
+            i++;
+        } else {
+            temp[index_t] = stu[j];
+            index_t++;
+            j++;
+        }
+    }
+
+    // 处理剩余的元素
+    while (i <= mid) {
+        temp[index_t] = stu[i];
+        index_t++;
+        i++;
+    }
+    while (j <= R) {
+        temp[index_t] = stu[j];
+        index_t++;
+        j++;
+    }
+
+    // 拷贝回数组
+    for (int k = 0; k < R - L + 1; k++) {
+        stu[L + k] = temp[k];
+    }
+}
+
 void merge_sort(int left, int right) {
     // TODO: 在这里添加你的代码
-    // I AM NOT DONE
+    if (left >= right) return;
+    int mid = (left + right) / 2;
+    printf("left = %d, right = %d, mid = %d, in merge_sort()\n", left, right, mid);
+    merge_sort(left, mid);
+    merge_sort(mid + 1, right);
+    merge(students, left, mid, right);
 }
 
 int main(void) {
